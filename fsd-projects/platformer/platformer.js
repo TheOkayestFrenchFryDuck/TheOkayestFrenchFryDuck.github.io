8f95d42d4cd748a4baa55092f8d875766a034d5b
TheOkayestFrenchFryDuck <mason.nguyen@jpschools.org>
2026-03-05 20:41:44 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,20 +27,37 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
+    createPlatform(500, 300, 200, 20,  "pink");
+    createPlatform(100, 550, 200, 20,  "yellow");
+    createPlatform(350, 450, 450, 20, "green"); 
+    createPlatform(100, 350, 200, 20,  "yellow"); 
+    createPlatform(1200, 105, 200, 20,  "aqua");
+    createPlatform(850, 250, 250, 20,  "purple");
+    createPlatform(900, 550, 200, 20,  "tomato");
+    createPlatform(1100, 450, 250, 20,  "orange");
+    createPlatform(1200, 1000, 300, 20,  "lightpink");
+    createPlatform(350, 650, 400, 100,  "lime");
+    createPlatform(1500, 225, 400, 100,  "hotpink");
+    createPlatform(1000, 150, 150, 20,  "aqua");
 
 
 
 
     // TODO 3 - Create Collectables
-
+    createCollectable("steve", 1000, 150, 0, 1,); 
+    createCollectable("diamond", 1100, 400, 0, 0,); 
+    createCollectable("max", 350, 400, 0, 0,); 
 
 
     
     // TODO 4 - Create Cannons
+    createCannon("top", 200, 1000);
+    createCannon("right", 450, 3000);
+    createCannon("left", 150, 2500);
 
 
     
